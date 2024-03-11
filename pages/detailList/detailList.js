691117ebfbf1987aae956389b4d582d27fc80a88
icefire2 <243508924@qq.com>
2024-03-11 16:07:29 +0800

    [dev]update

--- pages/detailList/detailList.js
+++ pages/detailList/detailList.js
@@ -58,39 +58,16 @@ Page({
     /**
      * 页面上拉触底事件的处理函数
      */
-<<<<<<< HEAD
     onReachBottom: function () {
         if (this.data.current_page < this.data.last_page) {
-            this.getList(this.data.tabbarNum, this.data.current_page);
+        this.getList(this.data.current_page);
         }
         else {
-            app.toastFun('已经没有了');
+        app.toastFun('已经没有了');
         }
     },
 
-    tabbarChange: function (e) {
-        let idx = e.currentTarget.dataset.idx;
-        this.setData({
-            tabbarNum: idx,
-            type_name: this.data.tabbar[idx],
-            details: []
-        })
-        this.getList(idx, 0);
-    },
-
-    getList(type, idx) {
-=======
-  onReachBottom: function () {
-    if (this.data.current_page < this.data.last_page) {
-      this.getList(this.data.current_page);
-    }
-    else {
-      app.toastFun('已经没有了');
-    }
-  },
-
     getList(idx) {
->>>>>>> 8728272b02493cd006c52da4e25bd4e03a2fc121
         let _this = this;
         let timestamp = new Date().getTime();
         let sign = util.getSign(timestamp);
@@ -105,41 +82,5 @@ Page({
           }).catch((err) => {
             console.log(err)
         })
-<<<<<<< HEAD
-        // let postdata = {
-        //     token: app.globalData.token,
-        //     page: idx + 1,
-        //     cur: type + 1
-        // };
-        // getRequest.post('index/Account/pointLog', postdata).then(function (res) {
-        //     _this.setData({
-        //         details: _this.data.details.concat(res.data.data),
-        //         current_page: res.data.current_page,
-        //         last_page: res.data.last_page
-        //     })
-        // }).catch(function (err) {
-        //     // console.log(err)
-        //     _this.setData({ loadState: true })
-        //     setTimeout(() => {
-        //         wx.navigateBack({
-        //             delta: 1,
-        //         })
-        //     }, 1000);
-        // })
-
-        getRequest.post('index/Account/point', { token: app.globalData.token }).then(function (res) {
-            if (res.data.is_vip) {
-                _this.setData({
-                    tabbar: ['积分', '余额', '电子卡', '实体卡'],
-                    point: res.data[_this.data.point_type[idx]]
-                })
-            } else {
-                _this.setData({
-                    point: res.data[_this.data.point_type[idx]]
-                })
-            }
-        })
-=======
->>>>>>> 8728272b02493cd006c52da4e25bd4e03a2fc121
     }
 })
